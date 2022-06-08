3f3d32ea3bc552c5a9dfa974c8434ab0c1e7a15f
Nayeon Park <49023717+Nayeon12@users.noreply.github.com>
2022-06-08 20:47:32 +0900

    Add files via upload

---
+++ Meditation_Recommendation2.dart
@@ -0,0 +1,162 @@
+import 'package:flutter/material.dart';
+
+class Mymeditation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.lightBlue.shade900,
+        ),
+      ),
+      home: new Scaffold(
+        appBar: new AppBar(
+          centerTitle: true,
+          title: new Text('명상 추천'),
+        ),
+        body: new SingleChildScrollView(
+          child: new Column(
+            children: <Widget>[
+              Padding(padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),),
+              Text.rich(
+                  TextSpan(
+                    children: <TextSpan> [
+                      TextSpan(
+                          text: '첫번째 명상 영상',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)
+                      ),
+                      TextSpan(
+                          text: '\n[명상음악]하루10분 만병을 치료해주는 ...',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                      TextSpan(
+                          text: '\n조회수 : 1192만회',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                      TextSpan(
+                          text: '\n영상주소 : https://www.youtube.com/watch?v=RtPwBk0pqKE',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                child: Row(children: <Widget>[
+                  Padding(padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),),
+                  SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Image.asset('lib/assets/video1.png')
+                  ),
+                  Padding(padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),),
+                  new Padding(
+                    padding: const EdgeInsets.all(20.0),
+                  ),
+                ],
+                ),
+              ),
+
+              Text.rich(
+                  TextSpan(
+                    children: <TextSpan> [
+                      TextSpan(
+                          text: '두번째 명상 영상',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)
+                      ),
+                      TextSpan(
+                          text: '\n티벳 대형 싱잉볼 연주(극저음) 명상음악/...',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                      TextSpan(
+                          text: '\n조회수 : 708만회',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                      TextSpan(
+                          text: '\n영상주소 : https://www.youtube.com/watch?v=2N4eTTipm9I',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center
+              ),
+
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                child: Row(children: <Widget>[
+                  Padding(padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),),
+                  SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Image.asset('lib/assets/video2.png')
+                  ),
+                  Padding(padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),),
+                  new Padding(
+                    padding: const EdgeInsets.all(20.0),
+                  ),
+                ],
+                ),
+              ),
+              Text.rich(
+                  TextSpan(
+                    children: <TextSpan> [
+                      TextSpan(
+                          text: '세번째 명상 영상',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20)
+                      ),
+                      TextSpan(
+                          text: '\n10분명상-당신의 삶에 명상이 필요할 때',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                      TextSpan(
+                          text: '\n조회수 : 50만회',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                      TextSpan(
+                          text: '\n영상주소 : https://www.youtube.com/watch?v=yiysD0Jl2Wo',
+                          style: TextStyle(
+                              fontSize: 17)
+                      ),
+                    ],
+                  ),
+                  textAlign: TextAlign.center
+              ),
+
+              Container(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 20),
+                child: Row(children: <Widget>[
+                  Padding(padding: const EdgeInsets.fromLTRB(0, 0, 30, 0),),
+                  SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Image.asset('lib/assets/video3.png')
+                  ),
+                  Padding(padding: const EdgeInsets.fromLTRB(0, 0, 10, 0),),
+                  new Padding(
+                    padding: const EdgeInsets.all(20.0),
+                  ),
+                ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
