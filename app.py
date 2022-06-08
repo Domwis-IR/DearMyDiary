8c24245af35adaa87cc1f5c91aa685b5f4028959
Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2022-06-08 19:40:59 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,jsonify
+
+from Depression_Diagnosis import Sentiment_Classifer, Symptom_Classifer
+app = Flask(__name__)
+
+
+
+@app.route('/',methods=['GET'])
+
+def analyze_diary():
+	data = request.get_json()
+	if 'image' not in data:
+        return "", 400
+
+    sentiment = Sentiment_Classifer.sentiment_classfier(data)
+	symptom = Symptom_Classifer.find_best_Ssymptom(data)
+
+    return sentiment, symptom, 200
+
+
+if __name__ == '__main__':
+
+	app.run()
